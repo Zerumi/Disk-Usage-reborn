d9e2dfae1b28b94c6e945a5a3d055f82edf854d3
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-12-17 14:41:34 +0300

    chore: add some static constants from source

--- app/src/main/java/io/github/diskusagereborn/DiskUsageApplication.kt
+++ app/src/main/java/io/github/diskusagereborn/DiskUsageApplication.kt
@@ -11,6 +11,13 @@ class DiskUsageApplication : Application() {
         fun getInstance() =
             instance ?:
             throw IllegalStateException("DiskUsage application is not created!")
+
+        const val RESULT_DELETE_CONFIRMED = 10
+        const val RESULT_DELETE_CANCELED = 11
+        const val STATE_KEY = "state"
+        const val KEY_KEY = "key"
+        const val DELETE_PATH_KEY = "path"
+        const val DELETE_ABSOLUTE_PATH_KEY = "absolute_path"
     }
 
     override fun onCreate() {
